7b4c8bed2e0cf432802f66ddfa555789fa6976e7
CitrusMaxima <2663753900@qq.com>
2018-06-13 16:21:41 +0800

    集成Mybatis

--- src/main/java/com/seckill/controller/SampleController.java
+++ src/main/java/com/seckill/controller/SampleController.java
@@ -1,7 +1,10 @@
 package com.seckill.controller;
 
+import com.seckill.domain.User;
 import com.seckill.result.CodeMsg;
 import com.seckill.result.Result;
+import com.seckill.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/demo")
 public class SampleController {
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping("/thymeleaf")
     public String thymeleaf(Model model) {
@@ -27,5 +33,19 @@ public class SampleController {
     @ResponseBody
     public Result<String> helloError() {
         return Result.error(CodeMsg.SERVER_ERROR);
+    }
+
+    @RequestMapping("/db/get")
+    @ResponseBody
+    public Result<User> dbGet() {
+        User user = userService.getById(1);
+        return Result.success(user);
+    }
+
+    @RequestMapping("/db/tx")
+    @ResponseBody
+    public Result<Boolean> dbTx() {
+        userService.tx();
+        return Result.success(true);
     }
 }
