ea9305a5841508f7803b9ee334f593d596b20618
CitrusMaxima <2663753900@qq.com>
2018-06-13 15:16:18 +0800

    秒杀系统项目环境搭建

--- src/main/java/com/seckill/controller/SampleController.java
+++ src/main/java/com/seckill/controller/SampleController.java
@@ -1,8 +1,11 @@
 package com.seckill.controller;
 
+import com.seckill.result.CodeMsg;
+import com.seckill.result.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/demo")
@@ -12,5 +15,17 @@ public class SampleController {
     public String thymeleaf(Model model) {
         model.addAttribute("name", "CitrusMaxima");
         return "hello";
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Result<String> hello() {
+        return Result.success("hello, CitrusMaxima");
+    }
+
+    @RequestMapping("/helloError")
+    @ResponseBody
+    public Result<String> helloError() {
+        return Result.error(CodeMsg.SERVER_ERROR);
     }
 }
