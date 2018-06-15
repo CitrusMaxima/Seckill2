a5dafbc5981f4774eb95f58edd5c77236d49e507
CitrusMaxima <2663753900@qq.com>
2018-06-15 14:51:49 +0800

    参数校验与异常处理

--- src/main/java/com/seckill/controller/LoginController.java
+++ src/main/java/com/seckill/controller/LoginController.java
@@ -1,9 +1,7 @@
 package com.seckill.controller;
 
-import com.seckill.result.CodeMsg;
 import com.seckill.result.Result;
 import com.seckill.service.SeckillUserService;
-import com.seckill.util.ValidatorUtil;
 import com.seckill.vo.LoginVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,8 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.thymeleaf.util.StringUtils;
 
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/login")
@@ -30,27 +28,10 @@ public class LoginController {
 
     @RequestMapping("/do_login")
     @ResponseBody
-    public Result<Boolean> doLogin(LoginVo loginVo) {
+    public Result<Boolean> doLogin(@Valid LoginVo loginVo) {
         logger.info(loginVo.toString());
-        // 参数校验
-        String passInput = loginVo.getPassword();
-        String mobile = loginVo.getMobile();
-        if (StringUtils.isEmpty(passInput)) {
-            return Result.error(CodeMsg.PASSWORD_EMPTY);
-        }
-        if (StringUtils.isEmpty(mobile)) {
-            return Result.error(CodeMsg.MOBILE_EMPTY);
-        }
-        if (!ValidatorUtil.isMobile(mobile)) {
-            return Result.error(CodeMsg.MOBILE_ERROR);
-        }
-
         // 登录
-        CodeMsg cm = seckillUserService.login(loginVo);
-        if (cm.getCode() == 0) {
-            return Result.success(true);
-        } else {
-            return Result.error(cm);
-        }
+        seckillUserService.login(loginVo);
+        return Result.success(true);
     }
 }
--- src/main/java/com/seckill/service/SeckillUserService.java
+++ src/main/java/com/seckill/service/SeckillUserService.java
@@ -2,6 +2,7 @@ package com.seckill.service;
 
 import com.seckill.dao.SeckillUserDao;
 import com.seckill.domain.SeckillUser;
+import com.seckill.exception.GlobalException;
 import com.seckill.result.CodeMsg;
 import com.seckill.util.MD5Util;
 import com.seckill.vo.LoginVo;
@@ -18,9 +19,9 @@ public class SeckillUserService {
         return seckillUserDao.getById(id);
     }
 
-    public CodeMsg login(LoginVo loginVo) {
+    public boolean login(LoginVo loginVo) {
         if (loginVo == null) {
-            return CodeMsg.SERVER_ERROR;
+            throw new GlobalException(CodeMsg.SERVER_ERROR);
         }
         String mobile = loginVo.getMobile();
         String formPass = loginVo.getPassword();
@@ -28,7 +29,7 @@ public class SeckillUserService {
         // 判断手机号是否存在
         SeckillUser user = getById(Long.parseLong(mobile));
         if (user == null) {
-            return CodeMsg.MOBILE_NOT_EXIST;
+            throw new GlobalException(CodeMsg.MOBILE_NOT_EXIST);
         }
 
         // 验证密码
@@ -36,8 +37,8 @@ public class SeckillUserService {
         String saltDB = user.getSalt();
         String calPass = MD5Util.formPassToDBPass(formPass, saltDB);
         if (!calPass.equals(dbPass)) {
-            return CodeMsg.PASSWORD_ERROR;
+            throw new GlobalException(CodeMsg.PASSWORD_ERROR);
         }
-        return CodeMsg.SUCCESS;
+        return true;
     }
 }
