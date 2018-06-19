c4d0b22517cd2b2825c35d14baab7a6528cf106f
CitrusMaxima <2663753900@qq.com>
2018-06-19 15:16:28 +0800

    商品详情页展示

--- src/main/java/com/seckill/controller/GoodsController.java
+++ src/main/java/com/seckill/controller/GoodsController.java
@@ -7,6 +7,7 @@ import com.seckill.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -29,6 +30,43 @@ public class GoodsController {
         model.addAttribute("goodsList", goodsList);
 
         return "goods_list";
+    }
+
+    @RequestMapping("/to_detail/{goodsId}")
+    public String toDetail(Model model, SeckillUser seckillUser, @PathVariable("goodsId") long goodsId) {
+
+        model.addAttribute("user", seckillUser);
+
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods", goods);
+
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+
+        System.out.println(goods.getStartDate());
+
+        int seckillStatus = 0;
+        int remainSeconds = 0;
+
+        if (now < startAt) {
+            // 秒杀未开始，倒计时
+            seckillStatus = 0;
+            remainSeconds = (int) ((startAt - now) / 1000);
+        } else if (now > endAt) {
+            // 秒杀已结束
+            seckillStatus = 2;
+            remainSeconds = -1;
+        } else {
+            // 秒杀进行中
+            seckillStatus = 1;
+            remainSeconds = 0;
+        }
+
+        model.addAttribute("seckillStatus", seckillStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+
+        return "goods_detail";
     }
 
 }
--- src/main/java/com/seckill/dao/GoodsDao.java
+++ src/main/java/com/seckill/dao/GoodsDao.java
@@ -2,6 +2,7 @@ package com.seckill.dao;
 
 import com.seckill.vo.GoodsVo;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -10,5 +11,8 @@ import java.util.List;
 public interface GoodsDao {
 
     @Select("select g.*, sg.stock_count, sg.start_date, sg.end_date, sg.seckill_price from seckill_goods sg left join goods g on sg.goods_id = g.id")
-    public List<GoodsVo> listGoodsVo();
+    List<GoodsVo> listGoodsVo();
+
+    @Select("select g.*, sg.stock_count, sg.start_date, sg.end_date, sg.seckill_price from seckill_goods sg left join goods g on sg.goods_id = g.id where g.id = #{goodsId}")
+    GoodsVo getGoodsVoByGoodsId(@Param("goodsId") long goodsId);
 }
