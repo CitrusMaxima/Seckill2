8ee9a233075f9acb95fc8658df3585757e39170f
CitrusMaxima <2663753900@qq.com>
2018-07-05 17:09:51 +0800

    代码格式调整

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,7 +1,8 @@
 //展示loading
-function g_showLoading(){
-	var idx = layer.msg('处理中...', {icon: 16,shade: [0.5, '#f5f5f5'],scrollbar: false,offset: '0px', time:100000}) ;  
-	return idx;
+function g_showLoading() {
+    var idx = layer.msg('处理中...', {icon: 16, shade: [0.5, '#f5f5f5'], scrollbar: false, offset: '0px', time: 100000});
+    return idx;
 }
+
 //salt
-var g_passsword_salt="27dkl4564sjsOIneo#*%UNF军嫂法儿sajOioaffj0&*(sfe"
+var g_passsword_salt = "27dkl4564sjsOIneo#*%UNF军嫂法儿sajOioaffj0&*(sfe"
