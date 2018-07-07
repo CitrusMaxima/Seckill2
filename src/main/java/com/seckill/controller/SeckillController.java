b4cea81822a513f10594931d294b784d0709513a
CitrusMaxima <2663753900@qq.com>
2018-07-07 22:07:34 +0800

    秒杀页面静态化

--- src/main/java/com/seckill/controller/SeckillController.java
+++ src/main/java/com/seckill/controller/SeckillController.java
@@ -4,6 +4,7 @@ import com.seckill.domain.OrderInfo;
 import com.seckill.domain.SeckillOrder;
 import com.seckill.domain.SeckillUser;
 import com.seckill.result.CodeMsg;
+import com.seckill.result.Result;
 import com.seckill.service.GoodsService;
 import com.seckill.service.OrderService;
 import com.seckill.service.SeckillService;
@@ -13,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/seckill")
@@ -31,35 +34,31 @@ public class SeckillController {
     @Autowired
     OrderService orderService;
 
-    @RequestMapping("/do_seckill")
-    public String doSeckill(Model model, SeckillUser seckillUser,
-                            @RequestParam("goodsId") long goodsId) {
+    @RequestMapping(value = "/do_seckill", method = RequestMethod.POST)
+    @ResponseBody
+    public Result<OrderInfo> doSeckill(Model model, SeckillUser seckillUser,
+                                       @RequestParam("goodsId") long goodsId) {
         model.addAttribute("user", seckillUser);
         if (seckillUser == null) {
-            return "login";
+            return Result.error(CodeMsg.SESSION_ERROR);
         }
 
         // 判断是否重复秒杀
         SeckillOrder order = orderService.getSeckillOrderByUserIdGoodsId(seckillUser.getId(), goodsId);
         if (order != null) {
-            model.addAttribute("errmsg", CodeMsg.SECKILL_REPEAT.getMsg());
-            return "seckill_fail";
+            return Result.error(CodeMsg.SECKILL_REPEAT);
         }
 
         // 判断库存
         GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
         int stock = goods.getStockCount();
         if (stock <= 0) {
-            model.addAttribute("errmsg", CodeMsg.SECKILL_OVER.getMsg());
-            return "seckill_fail";
+            return Result.error(CodeMsg.SECKILL_OVER);
         }
 
         // 执行秒杀，减库存，下订单，写入秒杀订单
         OrderInfo orderInfo = seckillService.doSeckill(seckillUser, goods);
-        model.addAttribute("orderInfo", orderInfo);
-        model.addAttribute("goods", goods);
-
-        return "order_detail";
+        return Result.success(orderInfo);
     }
 
 
